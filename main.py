7b74ec8cfcc95081bdaa8deb09dd5d1fef582caf
Aydar2007 <brawlball22@mail.ru>
2023-05-24 21:11:42 +0600

    commits

--- main.py
+++ main.py
@@ -12,7 +12,7 @@
 
 from dotenv import load_dotenv
 from aiogram.types import KeyboardButton,InlineKeyboardMarkup, InlineKeyboardButton,ReplyKeyboardMarkup
-import os,sqlite3,time,requests,logging,aioschedule
+import os,sqlite3,time,requests,logging,schedule
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram import Bot, Dispatcher, types, executor
 from states import Time,Title,IdMessage,Delete
@@ -25,10 +25,11 @@ dp = Dispatcher(bot, storage=storage)
 db = sqlite3.connect('database.db')
 cursor = db.cursor()
 cursor.execute("""CREATE TABLE  IF NOT EXISTS orders(
-    id INT,
-    id_list INT,
+    id INTEGER,
+    id_list INTEGER,
     title VARCHAR (500),
-    time VARCHAR (100) 
+    time VARCHAR (100),
+    every VARCHAR(100) 
 );
 """)
 cursor.connection.commit()
@@ -59,19 +60,25 @@ inline_buttons4 = [
 ]
 inline4 = InlineKeyboardMarkup().add(*inline_buttons4)
 
+inline_buttons5 = [
+    InlineKeyboardButton('Каждые ...(пример:Sunday,year) ',callback_data='inline_every'),
+]
+inline4 = InlineKeyboardMarkup().add(*inline_buttons5)
 
 @dp.callback_query_handler(lambda call: call)
 async def all_inline(call):
     if call.data == 'inline_creat':
-        await creats (call.message)
+        await creat (call.message)
     elif call.data == 'inline_delete':
-        await deletes(call.message,state=None)
+        await delete(call.message)
     elif call.data == "inline_idmes":
-        await idmessages (call.message,state=None)
+        await id_message (call.message)
     elif call.data == "inline_list":
-        await titles (call.message,state=None)
+        await title (call.message)
     elif call.data == "inline_times":
-        await timess (call.message,state=None)
+        await times (call.message)
+    elif call.data == "inline_every":
+        await every (call.message)
 
 @dp.message_handler(commands="start")
 async def start (message:types.Message):
@@ -90,58 +97,78 @@ async def start (message:types.Message):
     await message.answer(f"Привет{message.from_user.full_name}",reply_markup=inline1)
 
 @dp.message_handler(commands="creat")
-async def creats (message:types.Message):
+async def creat (message:types.Message):
     await message.answer("перейдем к следуещему для создания",reply_markup=inline2)
 
 @dp.message_handler(commands="idmessage", state=None)
-async def idmessages(message:types.Message):
+async def id_message(message:types.Message):
     await message.answer("Укажите id для сообщения(формат:1 или 2)",reply_markup=inline3)
     await IdMessage.set()
     
 @dp.message_handler(state=IdMessage)
-async def idmessages(message:types.Message,state:FSMContext):
+async def id_message(message:types.Message,state:FSMContext):
+    id_message = message.text
     cursor = db.cursor()
     cursor.execute(f"UPDATE orders SET id_list = {message.text} WHERE id = {message.from_user.id};")
     cursor.connection.commit() 
 
 @dp.message_handler(commands="title", state=None)
-async def titles (message:types.Message):
+async def title (message:types.Message):
     await message.answer("Укажите id для сообщения(формат:1 или 2)",reply_markup=inline4)
     await Title.set()
     
 @dp.message_handler(state=Title)
-async def titles(message:types.Message,state:FSMContext):
+async def title(message:types.Message,state:FSMContext):
+    title = message.text
     cursor = db.cursor()
     cursor.execute(f"UPDATE orders SET title = {message.text} WHERE id = {message.from_user.id};")
     cursor.connection.commit() 
 
 @dp.message_handler(commands="times", state=None)
-async def timess (message:types.Message):
+async def times(message:types.Message):
     await message.answer("Укажите id для сообщения(формат:1 или 2)",reply_markup=inline1)
     await Time.set()
     
 @dp.message_handler(state=Time)
-async def timess(message:types.Message,state:FSMContext):
+async def times(message:types.Message,state:FSMContext):
+    times = message.text
     cursor = db.cursor()
     cursor.execute(f"UPDATE orders SET time = {message.text} WHERE id = {message.from_user.id};")
     cursor.connection.commit() 
 
+@dp.message_handler(commands="every", state=None)
+async def every(message:types.Message):
+    await message.answer("Каждые ...(пример:Sunday,year)",reply_markup=inline1)
+    await Time.set()
+    
+@dp.message_handler(state="every")
+async def every(message:types.Message,state:FSMContext):
+    cursor = db.cursor()
+    cursor.execute(f"UPDATE orders SET every = {message.text} WHERE id = {message.from_user.id};")
+    cursor.connection.commit() 
+    schedule.every(every).at({times}).do({title})
+    while True:
+        schedule.run_pending()
+
 @dp.message_handler(commands="delete", state=None)
-async def deletes (message:types.Message):
+async def delete (message:types.Message):
     await message.answer("Какой номер сообщения удалить'без знака № ,просто цифра?'")
     await Delete.set()
 
 @dp.message_handler(state=Delete)
-async def deletes(message:types.Message,state:FSMContext):
+async def delete(message:types.Message,state:FSMContext):
 
     cursor = db.cursor()
     cursor.execute(f"DELETE title FROM orders WHERE id = {message.from_user.id},id_list = {message.text};")
     cursor.connection.commit() 
-    cursor = db.cursor()
+    cursor = db.cursor()    
     cursor.execute(f"DELETE time FROM orders WHERE id = {message.from_user.id},id_list = {message.text};")
     cursor.connection.commit() 
     cursor = db.cursor()
     cursor.execute(f"DELETE id_list FROM orders WHERE id = {message.from_user.id},id_list = {message.text};")
     cursor.connection.commit()
 
+
+
 executor.start_polling(dp)
+
